0a68676beee3aa71cb431472066d1e8790f88124
Hugo de Araujo <hugo.saraujo@tutanota.com>
2023-04-07 22:41:51 -0300

    Adição de métodos Depositar e Transferir na aplicação

--- ContaCorrente/ContaCorrente.cs
+++ ContaCorrente/ContaCorrente.cs
@@ -17,5 +17,31 @@ namespace ByteBank.ContaCorrente
         {
             this.saldo += valor;
         }
+        public bool Sacar(decimal valor) 
+        {
+            if (valor <= this.saldo)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            else 
+            {
+                return false;
+            }   
+        }
+
+        public bool Transferir(decimal valor, ContaCorrente destino)
+        { 
+            if (this.saldo < valor)
+            {
+                return false; 
+            }
+            else 
+            {
+                this.Sacar(valor);
+                destino.Depositar(valor);
+                return true; 
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -14,12 +14,55 @@ Console.WriteLine($"Saldo: R$ {String.Format("{0:00.00}", contaDoJoel.saldo)}");
 
 contaDoJoel.Depositar(500);
 
-
 Console.WriteLine();
 Console.WriteLine($"Titular: {contaDoJoel.titular}");
 Console.WriteLine($"Agência: {contaDoJoel.numeroAgencia}");
 Console.WriteLine($"Número de Conta: {contaDoJoel.conta}");
 Console.WriteLine($"Saldo: R$ {String.Format("{0:00.00}", contaDoJoel.saldo)}");
 
+ContaCorrente contaDoHugo = new ContaCorrente();
+contaDoHugo.titular = "Hugo";
+contaDoHugo.numeroAgencia = 1927;
+contaDoHugo.conta = "276543-2";
+contaDoHugo.saldo = 100;
+
+Console.WriteLine();
+Console.WriteLine($"Titular: {contaDoHugo.titular}");
+Console.WriteLine($"Agência: {contaDoHugo.numeroAgencia}");
+Console.WriteLine($"Número de Conta: {contaDoHugo.conta}");
+Console.WriteLine($"Saldo: R$ {String.Format("{0:00.00}", contaDoHugo.saldo)}");
+Console.WriteLine();
+
+contaDoHugo.Depositar(500);
+
+Console.WriteLine($"Titular: {contaDoHugo.titular}");
+Console.WriteLine($"Agência: {contaDoHugo.numeroAgencia}");
+Console.WriteLine($"Número de Conta: {contaDoHugo.conta}");
+Console.WriteLine($"Saldo: R$ {String.Format("{0:00.00}", contaDoHugo.saldo)}");
+Console.WriteLine();
+
+if (contaDoHugo.Sacar(100) == true)
+{
+    Console.WriteLine("Saque realizado com sucesso!!");
+    Console.WriteLine($"Saldo: R$ {String.Format("{0:00.00}", contaDoHugo.saldo)}");
+}
+else
+{
+    Console.WriteLine("Saldo indisponível!");
+}
+
+
+contaDoHugo.Transferir(125, contaDoJoel);
+
+Console.WriteLine($"Titular: {contaDoHugo.titular}");
+Console.WriteLine($"Agência: {contaDoHugo.numeroAgencia}");
+Console.WriteLine($"Número de Conta: {contaDoHugo.conta}");
+Console.WriteLine($"Saldo: R$ {String.Format("{0:00.00}", contaDoHugo.saldo)}");
+Console.WriteLine();
+
+Console.WriteLine($"Titular: {contaDoJoel.titular}");
+Console.WriteLine($"Agência: {contaDoJoel.numeroAgencia}");
+Console.WriteLine($"Número de Conta: {contaDoJoel.conta}");
+Console.WriteLine($"Saldo: R$ {String.Format("{0:00.00}", contaDoJoel.saldo)}");
 
 Console.ReadLine();
